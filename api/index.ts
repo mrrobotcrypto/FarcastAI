7508400e31bca7abc2e831d2b262a81a14cdab8d
mrrobotcrypto <hsn.dyn721@gmail.com>
2025-09-22 11:47:59 +0300

    chore(api): remove express aggregator; move to function-per-route

--- api/index.ts
+++
@@ -1,9 +0,0 @@
-// api/index.ts
-import { makeApp } from "../server/app";
-
-const app = makeApp();
-
-export default function handler(req: any, res: any) {
-  // @ts-ignore - Express signature ile uyumlu
-  return app(req, res);
-}
